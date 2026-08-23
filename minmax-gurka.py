e062d6ca97d00639d81dd90e4896c6688828147a
emilio <99819072+EccentricEmilio@users.noreply.github.com>
2026-08-23 21:44:36 +0200

    Add minmax placeholder code and minimax node jpeg

--- minmax-gurka.py
+++ minmax-gurka.py
@@ -1,3 +1,52 @@
+'''
+Placeholder for minimax algorithm
+
+function minimax(position, depth, maximizingPlayer)
+	if depth == 0 or game over in position
+		return static evaluation of position
+ 
+	if maximizingPlayer
+		maxEval = -infinity
+		for each child of position
+			eval = minimax(child, depth - 1, false)
+			maxEval = max(maxEval, eval)
+		return maxEval
+ 
+	else
+		minEval = +infinity
+		for each child of position
+			eval = minimax(child, depth - 1, true)
+			minEval = min(minEval, eval)
+		return minEval
+ 
+ 
+// initial call
+minimax(currentPosition, 3, true)function minimax(position, depth, maximizingPlayer)
+	if depth == 0 or game over in position
+		return static evaluation of position
+ 
+	if maximizingPlayer
+		maxEval = -infinity
+		for each child of position
+			eval = minimax(child, depth - 1, false)
+			maxEval = max(maxEval, eval)
+		return maxEval
+ 
+	else
+		minEval = +infinity
+		for each child of position
+			eval = minimax(child, depth - 1, true)
+			minEval = min(minEval, eval)
+		return minEval
+ 
+ 
+// initial call
+minimax(currentPosition, 3, true)
+'''
+
+
+
+
 import random
 from dataclasses import dataclass
 from copy import deepcopy
